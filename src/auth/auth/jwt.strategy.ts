698ae3e0fecea4b0ed141af7dca53b075b12dda7
ruslanabdulazimov <abdulazimov.03@mail.ru>
2025-02-08 21:53:33 +0500

    change imports2

--- src/auth/auth/jwt.strategy.ts
+++ src/auth/auth/jwt.strategy.ts
@@ -1,9 +1,8 @@
-import { ExtractJwt } from 'passport-jwt';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IUser } from 'src/types/types';
-import { Strategy } from 'passport-local';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
