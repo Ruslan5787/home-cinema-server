47f2134e330c59dfe25b6736f9bc72da887d0b61
ruslanabdulazimov <abdulazimov.03@mail.ru>
2025-02-08 21:41:26 +0500

    change import

--- src/auth/auth/jwt.strategy.ts
+++ src/auth/auth/jwt.strategy.ts
@@ -1,16 +1,17 @@
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ExtractJwt } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IUser } from 'src/types/types';
+import { Strategy } from 'passport-local';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
       secretOrKey: configService.get('JWT_SECRET'),
+      ignoreExpiration: false,
     });
   }
 
