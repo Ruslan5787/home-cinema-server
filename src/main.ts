b18b4363e222e06af118d727e7ed22f8982349f8
Ruslan Abdulazimov <abdulazimov.03@mail.ru>
2025-02-11 17:13:04 +0500

    change main

--- src/main.ts
+++ src/main.ts
@@ -2,37 +2,39 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // const app = await NestFactory.create(AppModule);
+
+  // app.setGlobalPrefix('api');
+
+  // app.use((req, res, next) => {
+  //   console.log(`Incoming request: ${req.method} ${req.path}`); // Логируем запрос
+  //   if (req.method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+  //     res.header(
+  //       'Access-Control-Allow-Methods',
+  //       'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  //     );
+  //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  //     res.header('Access-Control-Allow-Credentials', 'true');
+  //     return res.sendStatus(204); // Без тела
+  //   }
+  //   next();
+  // });
+
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'http://217.114.12.78'],
+  //   credentials: true,
+  //   methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
+  //   allowedHeaders: 'Content-Type, Authorization',
+  // });
+
+  // await app.listen(process.env.PORT ?? 3000);
+
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.path}`);
-    console.log('Headers:', req.headers);
-    next();
-  });
-
   app.setGlobalPrefix('api');
+  app.enableCors();
 
-  app.use((req, res, next) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-      res.header(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-      );
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.header('Access-Control-Allow-Credentials', 'true');
-      return res.sendStatus(204); // Отвечаем без тела
-    }
-    next();
-  });
-
-  app.enableCors({
-    origin: ['http://localhost:3000', 'http://217.114.12.78'],
-    credentials: true,
-    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
-
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
