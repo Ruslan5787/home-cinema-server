008aeab5c07dc7a6802eb05553ca924dbcd00921
Ruslan Abdulazimov <abdulazimov.03@mail.ru>
2025-02-11 16:51:45 +0500

    add cors 8

--- src/main.ts
+++ src/main.ts
@@ -5,39 +5,31 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    );
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204); // Отвечаем на preflight-запрос
-    }
-
-    next();
-  });
-
-  app.use((req, res, next) => {
     console.log(`Incoming request: ${req.method} ${req.path}`);
     console.log('Headers:', req.headers);
     next();
   });
 
   app.setGlobalPrefix('api');
+
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      );
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      return res.sendStatus(204); // Отвечаем без тела
+    }
+    next();
+  });
+
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = ['http://localhost:3000', 'http://217.114.12.78'];
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }, // Разрешаем запросы с клиента
-    credentials: true, // Разрешаем куки и авторизационные заголовки
-    methods: 'GET,POST,PUT,DELETE,PATCH',
+    origin: ['http://localhost:3000', 'http://217.114.12.78'],
+    credentials: true,
+    methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
